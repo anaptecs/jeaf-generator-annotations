a381985a7b0bbdd7c9aee44e84c28c975580b57b
JEAF Development Team <jeaf@anaptecs.de>
2026-09-23 07:21:08 +0200

    Improved annotations

--- src/main/java/com/anaptecs/jeaf/generator/annotations/BreakingChangeNotice.java
+++ src/main/java/com/anaptecs/jeaf/generator/annotations/BreakingChangeNotice.java
@@ -19,26 +19,55 @@ public @interface BreakingChangeNotice {
   String description() default "";
 
   /**
-   * Information since when a breaking change was announced for an element e.g. a concrete version or date.
+   * Specifies when a breaking change was first announced for an element, for example a specific version or date.
    */
   String since() default "";
 
   /**
-   * Version or date when a breaking change becomes active. </br>
+   * Version in which a breaking change becomes effective.</br>
    * </br>
    * <strong>Example:</strong>
    * <ul>
    * <li>A new parameter is introduced for a request. To not immediately break the API at first the parameter is
-   * optional. Using this field it can be defined when the parameter will be mandatory.</li>
+   * optional. Using this field it can be defined with which version the parameter will be mandatory.</li>
+   * </ul>
+   * <br/>
+   * <strong>UML Model / JEAF Generator:</strong>
+   * <ul>
+   * <li>Either taken directly from stereotype <code>BreakingChange</code> tagged value <code>activeWith</code> or</li>
+   * <li>from first incompatible version if using compatibility groups etc.</li>
    * </ul>
    */
   String activeWith() default "";
 
   /**
-   * Explicit date when the breaking change will be activated.
-   *
-   * The expected date format is `yyyy-mm-dd`.
+   * The date on which the breaking change becomes effective in production environments.<br/>
+   * <br/>
+   * A go-live date does not necessarily mean that existing clients will stop working immediately. Depending on the
+   * release policy, a transition or compatibility period may be provided (see {@link #endOfCompatibilityPhase()}).<br/>
+   * <br/>
+   * The expected date format is <strong><code>yyyy-mm-dd</code></strong>.<br/>
+   * <br/>
+   * <strong>UML Model / JEAF Generator:</strong>
+   * <ul>
+   * <li>Either taken directly from stereotype <code>BreakingChange</code> tagged value <code>goLiveDate</code> or</li>
+   * <li>from first incompatible version if using compatibility groups etc.</li>
+   * </ul>
    */
-  String activationDate() default "";
+  String goLiveDate() default "";
 
+  /**
+   * The date on which the compatibility phase for a breaking change ends in production environments.<br/>
+   * <br/>
+   * Consumers must have adapted their code by this date to avoid compatibility issues once the compatibility phase has
+   * ended.<br/>
+   * <br/>
+   * The expected date format is <strong><code>yyyy-mm-dd</code></strong>. <br/>
+   * <br/>
+   * <strong>UML Model / JEAF Generator:</strong>
+   * <ul>
+   * <li>Taken from <code>endOfLifeDate</code> from compatibility info
+   * </ul>
+   */
+  String endOfCompatibilityPhase() default "";
 }
--- src/main/java/com/anaptecs/jeaf/generator/annotations/DeprecationNotice.java
+++ src/main/java/com/anaptecs/jeaf/generator/annotations/DeprecationNotice.java
@@ -20,19 +20,56 @@ public @interface DeprecationNotice {
   String description() default "";
 
   /**
-   * Information since when an element is deprecated e.g. a concrete version or date.
+   * Specifies when a deprecation was first announced for an element, for example a specific version or date.
    */
   String since() default "";
 
   /**
-   * Information when a deprecated element will no longer be available e.g. a version or date.
+   * Version in which a deprecation will be removed.<br/>
+   * </br>
+   * <strong>Example:</strong>
+   * <ul>
+   * <li>An existing parameter in a request should not be removed. To not immediately break the API at first the
+   * parameter is only marked as deprecated. Using this field it can be defined starting from which version the
+   * parameter will not be supported any more.</li>
+   * </ul>
+   * <br/>
+   * <strong>UML Model / JEAF Generator:</strong>
+   * <ul>
+   * <li>Either taken directly from stereotype <code>JEAFDeprecated</code> tagged value <code>removedWith</code> or</li>
+   * <li>from first incompatible version if using compatibility groups etc.</li>
+   * </ul>
    */
   String removedWith() default "";
 
   /**
-   * Explicit date when the deprecation will be removed from the code base.
-   *
-   * The expected date format is `yyyy-mm-dd`.
+   * The date on which the deprecation becomes effective in production environments.<br/>
+   * <br/>
+   * A go-live date does not necessarily mean that existing clients will stop working immediately. Depending on the
+   * release policy, a transition or compatibility period may be provided (see {@link #endOfCompatibilityPhase()}).<br/>
+   * <br/>
+   * The expected date format is <strong><code>yyyy-mm-dd</code></strong>. <br/>
+   * <br/>
+   * <strong>UML Model / JEAF Generator:</strong>
+   * <ul>
+   * <li>Either taken directly from stereotype <code>JEAFDeprecated</code> tagged value <code>goLiveDate</code> or</li>
+   * <li>from first incompatible version if using compatibility groups etc.</li>
+   * </ul>
    */
-  String removalDate() default "";
+  String goLiveDate() default "";
+
+  /**
+   * The date on which the compatibility phase for a deprecation ends in production environments.<br/>
+   * <br/>
+   * Consumers must have adapted their code by this date to avoid compatibility issues once the compatibility phase has
+   * ended.<br/>
+   * <br/>
+   * The expected date format is <strong><code>yyyy-mm-dd</code></strong>.<br/>
+   * <br/>
+   * <strong>UML Model / JEAF Generator:</strong>
+   * <ul>
+   * <li>Taken from <code>endOfLifeDate</code> from compatibility info
+   * </ul>
+   */
+  String endOfCompatibilityPhase() default "";
 }
